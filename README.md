86ac4f6a8a5118afe03dd7fb115c45e118a33cdd
ijalandhika <ijalandhika@gmail.com>
2019-11-18 15:53:09 +0700

    - add mongo to connect database - modular resolver & schema

---
+++ README.md
@@ -0,0 +1,3 @@
+Thanks to Academind
+
+url: https://www.youtube.com/watch?v=7giZGFDGnkc&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_&index=1
--- app.js
+++ app.js
@@ -1,61 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp  = require('express-graphql');
-const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 
 const app = express();
 
-const events = [];
-
 app
     .use(bodyParser.json())
     .use('/graphql', graphqlHttp({
-        schema: buildSchema(`
-            type Event {
-                _id: ID!
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-
-            type RootQuery  {
-                events: [Event!]!
-            }
-
-            input EventInput {
-                title: String!
-                description: String!
-                price: Float!
-            }
-
-            type RootMutation {
-                createEvent(eventInput: EventInput): Event
-            }
-
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `), 
-        rootValue: {
-            events: () => {
-                return events
-            },
-            createEvent: (args) => {
-                const event =  {
-                    _id: Math.random().toString(),
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price,
-                    date: new Date().toISOString()
-                }
-                events.push(event);
-                return event
-            }
-        },
+        schema: graphQlSchema, 
+        rootValue: graphQlResolvers,
         graphiql: true
     }));
 
-app.listen(3000);
+const connection  = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongotest-7qtmn.gcp.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+
+mongoose
+.connect( connection, { useNewUrlParser: true, useUnifiedTopology: true } )
+.then(() => {
+    app.listen(3000);
+}).catch((err) => {
+    console.log(err);
+});
+
